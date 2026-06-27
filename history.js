691f0b15f8ad7de918fb70bd097d77241eb8f67e
HasniAlchemy <hasni.kesuma26@guru.sma.belajar.id>
2026-06-28 00:40:00 +0700

    Update history.js

--- history.js
+++ history.js
@@ -1,15 +1,25 @@
 window.onload = async function () {
-  const { data: keuangan, error } = await supabaseClient
+  const tbody = document.getElementById("history-data");
+  tbody.innerHTML = "";
+
+  const { data, error } = await supabaseClient
     .from("keuangan")
     .select("*")
     .order("tanggal", { ascending: false });
 
-  console.log("History:", keuangan, error);
+  console.log("DATA HISTORY:", data);
+  console.log("ERROR:", error);
 
-  const tbody = document.getElementById("history-data");
-  tbody.innerHTML = "";
+  if (error) {
+    tbody.innerHTML = `
+      <tr>
+        <td colspan="13">Gagal mengambil data</td>
+      </tr>
+    `;
+    return;
+  }
 
-  if (!keuangan || keuangan.length === 0) {
+  if (!data || data.length === 0) {
     tbody.innerHTML = `
       <tr>
         <td colspan="13">Belum ada data</td>
@@ -18,7 +28,7 @@ window.onload = async function () {
     return;
   }
 
-  keuangan.forEach(item => {
+  data.forEach(item => {
     tbody.innerHTML += `
       <tr>
         <td>
@@ -26,6 +36,19 @@ window.onload = async function () {
             ${item.tanggal}
           </a>
         </td>
-        <td>${item.uang_masuk || 0}</td>
-        <td>${item.bonus || 0}</td>
-        <td>${item
+        <td>${Number(item.uang_masuk) || 0}</td>
+        <td>${Number(item.bonus) || 0}</td>
+        <td>${Number(item.shopee) || 0}</td>
+        <td>${Number(item.qris) || 0}</td>
+        <td>${Number(item.pengeluaran) || 0}</td>
+        <td>${Number(item.pengeluaran1) || 0}</td>
+        <td>${Number(item.pengeluaran2) || 0}</td>
+        <td>${Number(item.pengeluaran3) || 0}</td>
+        <td>${Number(item.pengeluaran4) || 0}</td>
+        <td>${Number(item.pengeluaran5) || 0}</td>
+        <td>${Number(item.total_pengeluaran) || 0}</td>
+        <td>${Number(item.sisa) || 0}</td>
+      </tr>
+    `;
+  });
+};

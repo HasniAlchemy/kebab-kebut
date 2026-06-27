b8876271d4851ceff72077ee4a5b28f9da90c41a
HasniAlchemy <hasni.kesuma26@guru.sma.belajar.id>
2026-06-27 23:17:17 +0700

    Update history.js

--- history.js
+++ history.js
@@ -2,24 +2,21 @@ window.onload = async function(){
   const { data } = await supabaseClient
     .from("keuangan")
     .select("*")
-    .order("tanggal", { ascending:false });
+    .order("tanggal",{ascending:false});
 
-  let html = "<table><tr><th>Tanggal</th><th>Shopee</th><th>QRIS</th><th>Pengeluaran</th><th>Uang Masuk</th><th>Sisa</th></tr>";
+  let html = "<table><tr><th>Tanggal</th><th>Uang Masuk</th><th>Total Pengeluaran</th><th>Sisa</th></tr>";
 
-  data.forEach(item => {
+  data.forEach(item=>{
     html += `
-      <tr>
-        <td>${item.tanggal}</td>
-        <td>${item.shopee}</td>
-        <td>${item.qris}</td>
-        <td>${item.pengeluaran}</td>
-        <td>${item.uang_masuk}</td>
-        <td>${item.sisa}</td>
-      </tr>
-    `;
+    <tr>
+      <td><a href="detail.html?tanggal=${item.tanggal}">${item.tanggal}</a></td>
+      <td>${item.uang_masuk}</td>
+      <td>${item.total_pengeluaran}</td>
+      <td>${item.sisa}</td>
+    </tr>`;
   });
 
   html += "</table>";
 
-  document.getElementById("history-data").innerHTML = html;
+  document.getElementById("history-list").innerHTML = html;
 }
